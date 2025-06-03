12146d5a9b6d96176c0369ad2204d8d4648c31c9
tonny-da-creater <toni.belovanov96@mail.ru>
2025-06-03 15:42:19 +0300

    Добавил шрифты, переменные, создал меню под все устройства

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,19 @@
 import '../styles/style.scss';
 
-
 let navToggle = document.querySelector('.nav-toggle');
 let navMenu = document.querySelector('.nav-menu');
+let navLinks = document.querySelectorAll('.nav-menu__link');
+let currentPath = window.location.hash;
+let body = document.body;
 
 navToggle.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  navToggle.classList.toggle('active');
+  body.classList.toggle('menu-open');
+});
+
+navLinks.forEach(link => {
+  if (link.getAttribute('href') === currentPath) {
+    link.classList.add('active');
+  }
 });
